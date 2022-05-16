11baf373a72ba099ef22463d1e0489c5d7c46b02
Barashkov <nebunohu@gmail.com>
2022-05-16 15:17:11 +0300

    Form validation added

--- client/src/components/form/form.tsx
+++ client/src/components/form/form.tsx
@@ -1,36 +1,50 @@
-import { Button, FormControl, FormHelperText, OutlinedInput, Select } from "@mui/material";
-import { FC, ChangeEvent, FormEvent, useState } from "react";
+import { Button, FormControl, FormHelperText, MenuItem, OutlinedInput, Select, SelectChangeEvent } from "@mui/material";
+import { FC, ChangeEvent, FormEvent, useState,  } from "react";
 
 import { API_BASE_URL } from "../../consts";
 
 import styles from './form.module.scss';
 
+import Config from "../../config";
+
 const Form: FC = () => {
-  const [formState, setFormState] = useState({phone: ''});
+  const defaultCode = Config.phoneCodes.find(el => el.country === "Russia");
+  const [formState, setFormState] = useState({phone: '', code: defaultCode ? defaultCode.code : "", error: {flag: false, message: ""}});
+  // const [isError, setIsError] = useState(false);
+  //const selectRef = useRef<HTMLDivElement>(null);
+  //const defaultSelectCode = ;
 
   const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
     setFormState({...formState, phone: e.target.value});
   };
 
+  const onSelectChangeHandler = (e: SelectChangeEvent) => {
+    setFormState({...formState, code: e.target.value});
+  };
+
   const onSubmitHandler = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    //const target = e.target as HTMLInputElement;
-    //const target = e.target as Element;
-    try {
-      await fetch(`${API_BASE_URL}/users/add`, {
-      // const res = fetch('http://localhost:3001/api/users/test', {
-        method: "POST",
-        headers:{
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          phone: formState.phone
-        })
-      });
-      // const result = await res.json();
-      //   console.log("Успех: ", JSON.stringify(result));
-    } catch (error: any) {
-      console.log(error.message);
+    if ( formState.phone.match(/^\d{3,10}$/) ) {
+      if (formState.error.flag) setFormState({...formState, error: {flag: false, message: ""}});
+      try {
+        const res = await fetch(`${API_BASE_URL}/users/add`, {
+          method: "POST",
+          headers:{
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({ phone: formState.code + formState.phone })
+        });
+
+        if (!res.ok) {
+          const body = await res.json() ;
+          setFormState({ ...formState, error: { flag: true, message: body.message ? body.message : "" }});
+        }
+      } catch (error: any) {
+        console.log(error.message);
+        
+      }
+    } else {
+      setFormState({...formState, error: {flag: true, message: "Номер введен неверно"}});
     }
   };
 
@@ -39,16 +53,22 @@ const Form: FC = () => {
       <FormControl>
         <Select 
           size="small"
-        />
+          value={formState.code}
+          onChange={onSelectChangeHandler} 
+        >
+          {Config.phoneCodes.map((el, index) => (<MenuItem value={el.code} key={index}>{el.code}</MenuItem>))}
+        </Select>
         <FormHelperText></FormHelperText>
       </FormControl>
       <FormControl>
         <OutlinedInput 
           name="phone"
+          id="phone"
           onChange={onChangeHandler} 
           size="small"
+          error={formState.error.flag}
         />
-        <FormHelperText></FormHelperText>
+        <FormHelperText id="phone" error={formState.error.flag}>{formState.error.message}</FormHelperText>
       </FormControl>
       <Button 
         type="submit"
---
+++ client/src/config.ts
@@ -0,0 +1,18 @@
+const Config = {
+  phoneCodes: [
+    {
+      country: "USA",
+      code: "+1",
+    },
+    {
+      country: "Russia",
+      code: "+7",
+    },
+    {
+      country: "Ukraine",
+      code: "+380",
+    },
+  ]
+};
+
+export default Config;
