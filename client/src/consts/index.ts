a45517c7dcd6ad577d9592e9f956c4cd04d5ddab
Barashkov <nebunohu@gmail.com>
2022-05-16 17:06:35 +0300

    Modifications for deployment

--- client/src/consts/index.ts
+++ client/src/consts/index.ts
@@ -1,2 +1,5 @@
-export const API_BASE_URL = "http://localhost:3001/api";
-export const WS_API_URL = "ws://localhost:3002";
+// export const API_BASE_URL = "http://localhost:3001/api";
+// export const WS_API_URL = "ws://localhost:3002";
+
+export const API_BASE_URL = "https://test-task-superkassa.herokuapp.com/api";
+export const WS_API_URL = "wss://test-task-superkassa.herokuapp.com";
